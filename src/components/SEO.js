92de4322443e0ab208d87d74473e357fa9675385
GetmevaDev <devteam@getmeva.com>
2023-06-21 15:44:38 +0600

    test

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -18,22 +18,24 @@ function SEO({
       <title> {title && "Traffic Ticket Lawyer New York"} </title>
       {title && <meta property="og:title" content={title} />}
 
-      <meta name="description" content={description} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
-      <meta property="og:type" content={type} />
+      {description && <meta name="description" content={"description"} />}
+      {description && (
+        <meta property="og:description" content={"description"} />
+      )}
+      {image && <meta property="og:image" content={"image"} />}
+      {type && <meta property="og:type" content={type} />}
 
       {/* Twitter Meta Tags */}
-      <meta name="twitter:card" content={twitterCard} />
-
-      <meta property="twitter:domain" content={twitterDomain} />
-
-      <meta property="twitter:url" content={twitterUrl} />
-      <meta name="twitter:title" content={twitterTitle} />
-
-      <meta name="twitter:description" content={twitterDescription} />
-
-      <meta name="twitter:image" content={twitterImage} />
+      {twitterCard && <meta name="twitter:card" content={twitterCard} />}
+      {twitterDomain && (
+        <meta property="twitter:domain" content={twitterDomain} />
+      )}
+      {twitterUrl && <meta property="twitter:url" content={twitterUrl} />}
+      {twitterTitle && <meta name="twitter:title" content={twitterTitle} />}
+      {twitterDescription && (
+        <meta name="twitter:description" content={twitterDescription} />
+      )}
+      {twitterImage && <meta name="twitter:image" content={twitterImage} />}
     </Helmet>
   );
 }
